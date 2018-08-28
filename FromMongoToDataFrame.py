e3f26e38b6ba976888921cd83fdb79d18606b45c
jang0820 <jang0820@gmail.com>
2018-08-28 11:00:10 +0800

    add comment

--- FromMongoToDataFrame.py
+++ FromMongoToDataFrame.py
@@ -14,19 +14,19 @@ def connect_mongo():  #連線資料庫
     db = client[MONGO_DB]
     collection = db[MONGO_COLLETION]
 
-connect_mongo()
+connect_mongo()  #呼叫連線資料庫函式
 query = { 'stockno' : '2892' }
 cursor = collection.find(query)  #依query查詢資料
-stock =  pd.DataFrame(list(cursor))
+stock =  pd.DataFrame(list(cursor))  #轉換成DataFrame
 del stock['_id']  #刪除欄位_id
 print(stock)
 indexlist = []
-for i in range(len(stock)):
+for i in range(len(stock)):  #建立日期串列
     indexlist.append(stock['date'][i])  #stock['date'][i]為datetime.date物件
 stock.index = indexlist  #索引值改成日期
 stock = stock.drop(['date'],axis = 1)  #刪除日期欄位
 mlist = []
-for item in stock.index:
+for item in stock.index:   #建立月份串列
     mlist.append(item.month)
 stock['month'] = mlist  #新增月份欄位
 #print(stock)
@@ -36,15 +36,15 @@ for item in result[result.close > 21]:  #收盤價大於21元
     
 print(result[(result.index >= "2018-06-01") & (result.index <= "2018-06-30") & (result.close >= 21)])  #六月份大於21元
 
-tmp = result.sort_values(by = 'close', ascending=False)
+tmp = result.sort_values(by = 'close', ascending=False)   #依照收盤價排序
 print(tmp[:3])  #取收盤價前三高
 
 print(result.loc["2018-06-01":"2018-06-30"])  #只顯示2018六月份
 
 print(result.loc["2018-01-01":"2018-12-31"].groupby('month').close.count())  #2018每個月幾個營業日
 
-print(result.loc["2018-01-01":"2018-12-31"].groupby('month').shares.sum())  #每個月累計成交股數
+print(result.loc["2018-01-01":"2018-12-31"].groupby('month').shares.sum())  #2018每個月累計成交股數
 
-result.loc["2018-01-01":"2018-12-31"].groupby('month').shares.sum().plot()  #月累計成交股數圖
+result.loc["2018-01-01":"2018-12-31"].groupby('month').shares.sum().plot()  #2018月累計成交股數圖
 pp.ylabel('shares')
 pp.title('month of shares')
